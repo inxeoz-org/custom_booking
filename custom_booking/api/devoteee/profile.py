feb809eabbabefdbcc24746d783eb1c9a9b7ba1a
inxeoz <inxeoz@inxeoz.com>
2025-12-01 22:31:23 +0530

    update phone number

--- custom_booking/api/devoteee/profile.py
+++ custom_booking/api/devoteee/profile.py
@@ -1,6 +1,9 @@
+from typing import Dict
+
 import frappe
 from frappe.utils import random_string
 
+from ..sms.sms import send_sms_otp, verify_sms_otp
 from ..token.token import token_auth
 
 
@@ -26,12 +29,23 @@ def profile():
 		"aadhar": customer.custom_aadhar,
 		"location": customer.custom_location,
 		"companion": customer.custom_companion,
+		"phone": customer.custom_phone,
 	}
 
 
 @frappe.whitelist(allow_guest=True)
 @token_auth
-def update_profile(devoteee_name: str, email: str, gender: str, dob: str, aadhar: str, location: str):
+def update_profile(
+	devoteee_name: str | None = None,
+	email: str | None = None,
+	gender: str | None = None,
+	dob: str | None = None,
+	aadhar: str | None = None,
+	location: str | None = None,
+	companion: Dict | None = None,
+	phone: int | None = None,
+	otp: str | None = None,
+):
 	customer_id = frappe.local.form_dict.get("customer_id")
 
 	if not customer_id:
@@ -52,6 +66,16 @@ def update_profile(devoteee_name: str, email: str, gender: str, dob: str, aadhar
 		customer.custom_aadhar = aadhar
 	if location:
 		customer.custom_location = location
+	if phone:
+		if otp is None:
+			status = send_sms_otp(phone)
+			return status["message"]
+		else:
+			verification_status = verify_sms_otp(phone, otp)
+			if verification_status["VERIFIED"]:
+				customer.custom_phone = phone
+			else:
+				return verification_status["message"]
 
 	customer.save()
 	return profile()
