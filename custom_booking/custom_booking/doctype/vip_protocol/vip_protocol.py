dc700985170911952a2a26155e691d1b91fe832a
inxeoz <inxeoz@inxeoz.com>
2025-12-08 01:55:41 +0530

    protocol_level

--- custom_booking/custom_booking/doctype/vip_protocol/vip_protocol.py
+++ custom_booking/custom_booking/doctype/vip_protocol/vip_protocol.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2025, inxeoz and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class VipProtocol(Document):
 	pass
+
+
+@frappe.whitelist(allow_guest=True)
+def protocol_info(protocol_name):
+	try:
+		data = frappe.db.get_value(
+			"Vip Protocol", protocol_name, ["protocol_name", "description", "protocol_level"], as_dict=True
+		)
+
+		if not data:
+			frappe.throw("Protocol not found")
+
+		return data
+	except Exception as e:
+		frappe.throw(str(e))
+
+
+@frappe.whitelist(allow_guest=True)
+def protocol_list():
+	return frappe.get_all("Vip Protocol", fields=["name", "protocol_name", "description", "protocol_level"])
