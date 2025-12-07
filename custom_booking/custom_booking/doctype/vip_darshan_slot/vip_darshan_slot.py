065d22a0ee1f0d53395d718a061b038a64ebe5d2
inxeoz <inxeoz@inxeoz.com>
2025-12-07 15:08:02 +0530

    create slot and appointment table

--- custom_booking/custom_booking/doctype/vip_darshan_slot/vip_darshan_slot.py
+++ custom_booking/custom_booking/doctype/vip_darshan_slot/vip_darshan_slot.py
@@ -18,29 +18,29 @@ class VipDarshanSlot(Document):
 			super().autoname()
 
 
+def create_slot(slot_date):
+	id = to_frappe_date(slot_date, format="%d-%m-%Y-1")
+
+	# if exists → return directly
+	if frappe.db.exists("Vip Darshan Slot", id):
+		slot_date_doc = frappe.get_doc("Vip Darshan Slot", id)
+		return slot_date_doc
+	else:
+		default_slot_info = frappe.get_doc("Vip Darshan Slot Info")
+		slot_date_doc = frappe.new_doc("Vip Darshan Slot")
+		slot_date_doc.slot_date = to_frappe_date(slot_date)
+		slot_date_doc.slot_info = default_slot_info.slot_info
+		slot_date_doc.insert(ignore_permissions=True)
+		return slot_date_doc
+
+
 @frappe.whitelist(allow_guest=True)
 def get_vip_darshan_slots(slot_date):
-	try:
-		# convert date → custom ID
-		id = to_frappe_date(slot_date, format="%d-%m-%Y-1")
+	slot_date_doc = create_slot(slot_date)
+	# filter fields
+	selected_fields = [
+		{"slot_name": row.slot_name, "current_capacity": row.current_capacity}
+		for row in slot_date_doc.slot_info
+	]
 
-		# if exists → return directly
-		if frappe.db.exists("Vip Darshan Slot", id):
-			slot_date_doc = frappe.get_doc("Vip Darshan Slot", id)
-		else:
-			default_slot_info = frappe.get_doc("Vip Darshan Slot Info")
-			slot_date_doc = frappe.new_doc("Vip Darshan Slot")
-			slot_date_doc.slot_date = to_frappe_date(slot_date)
-			slot_date_doc.slot_info = default_slot_info.slot_info
-			slot_date_doc.insert()
-
-		# filter fields
-		selected_fields = [
-			{"slot_name": row.slot_name, "current_capacity": row.current_capacity}
-			for row in slot_date_doc.slot_info
-		]
-
-		return selected_fields
-
-	except Exception as e:
-		frappe.throw(str(e))
+	return selected_fields
